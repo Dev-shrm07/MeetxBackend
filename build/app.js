02192cc1c5f3ccd82b1e38e56e533560fd84f339
daddy <sharmakavyaop702@gmail.com>
2023-07-30 10:42:23 +0530

    changes

--- build/app.js
+++ build/app.js
@@ -55,6 +55,7 @@ app.use((0, express_session_1.default)({
     }),
 }));
 app.use((0, cors_1.default)({
+    origin: 'http://localhost:3000',
     credentials: true
 }));
 app.use((0, morgan_1.default)("dev"));
